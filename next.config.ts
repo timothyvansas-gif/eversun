a90d4b5c9abd9a4490c43fbd95f2051995d15a30
timothyvansas-gif <timothyvansas@gmail.com>
2026-05-02 21:09:01 +0200

    chore: update allowed development origin IP address in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.2.48", "localhost:3000"],
+  allowedDevOrigins: ["192.168.2.53", "localhost:3000"],
 };
 
 export default nextConfig;
